7501e7a7d03a4a4c743ed9cc03eca66d92d3870a
noeortizdev <ingnoeortiz@gmail.com>
2024-12-09 14:54:57 -0600

    BLL - Camiones (Read)

---
+++ BLL/BL_Camiones.cs
@@ -0,0 +1,25 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VO;
+
+namespace BLL
+{
+    public class BL_Camiones
+    {
+        // Create
+
+        // Read
+        public static List<Camiones_VO> Get_Camiones(params object[] parametros)
+        {
+            return DAL_Camiones.Get_Camiones(parametros);
+        }
+
+        // Update
+
+        // Delete
+    }
+}
