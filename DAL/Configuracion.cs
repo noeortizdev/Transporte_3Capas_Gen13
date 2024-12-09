5e2a23925f091e5de848efbe7a4c3bc00293227d
noeortizdev <ingnoeortiz@gmail.com>
2024-12-09 12:52:50 -0600

    Cadena de conexión

---
+++ DAL/Configuracion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Configuracion
+    {
+        // Cadena de Conexión
+        // Data Source = nombre del servidor de BD
+        // localhost
+        // .
+        // Nombre de mi instancia
+        // Initial Catalog = nombre de la BD
+        // Integrated Security = true (Credenciales de la máquina)
+        // = false (Credenciales de acceso)
+        // Se habilitan los campos de
+        // User =;
+        // Password =;
+
+        static string _cadenaConexion = @"Data Source = THINKPAD-PC\SQLEXPRESS;
+                                          Initial Catalog = Transportes;
+                                          Integrated Security = true;";
+
+        // Encapsulamiento
+        public static string CadenaConexion
+        {
+            get
+            {
+                return _cadenaConexion;
+            }
+        }
+    }
+}
--- VO/Class1.cs
+++ VO/Class1.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace VO
 {
-    public class Class1
+    public class Class
     {
+        
     }
 }
